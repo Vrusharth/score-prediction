cd37bbd8ef07057b5372f959c6891d161fb08eb4
Vrusharth Nirmal <103025007+Vrusharth@users.noreply.github.com>
2024-04-23 03:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sklearn
 import pandas as pd
 import pickle
 pipe = pickle.load(open('pipe.pkl','rb'))
-st.title('IPL Win Predictor')
+st.title('Poonam's DWM Project')
 teams = sorted(['Sunrisers Hyderabad',
  'Mumbai Indians',
  'Royal Challengers Bangalore',
